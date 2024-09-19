6d5f4c8629bb03a2a3ea5c17565d0bd0d307dbc1
Cao Duy Nguyen (까위쮜) <130331097+dennisnguyen9722@users.noreply.github.com>
2024-09-19 15:30:33 +0700

    third commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import envConfig from "../../config";
+import envConfig from "../config";
 console.log(envConfig)
 
 const geistSans = localFont({
